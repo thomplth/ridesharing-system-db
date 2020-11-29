c57743df10c93d48f4561c1c87ec9c0b5c5fdcb0
thomplth <29663345+thomplth@users.noreply.github.com>
2020-11-30 02:28:32 +0800

    Minor bug fix.

--- RideSharing/src/ridesharing/SystemAdministrator.java
+++ RideSharing/src/ridesharing/SystemAdministrator.java
@@ -218,30 +218,3 @@ public class SystemAdministrator {
         csv.close();
     }
 }
-
-/*
-BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
-while ((row = csvReader.readLine()) != null) {
-    String[] data = row.split(",");
-    // do something with the data
-}
-csvReader.close();
-
-public static void main(String[] args)   
-{  
-String line = "";  
-String splitBy = ",";  
-try   
-{  
-BufferedReader br = new BufferedReader(new FileReader("CSVDemo.csv"));  
-while ((line = br.readLine()) != null)   //returns a Boolean value  
-{  
-String[] employee = line.split(splitBy);    // use comma as separator  
-System.out.println("Employee [First Name=" + employee[0] + ", Last Name=" + employee[1] + ", Designation=" + employee[2] + ", Contact=" + employee[3] + ", Salary= " + employee[4] + ", City= " + employee[5] +"]");  
-}  
-}   
-catch (IOException e)   
-{  
-e.printStackTrace();  
-}  
-*/
